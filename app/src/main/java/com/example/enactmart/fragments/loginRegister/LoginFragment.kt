9375d1cbd88075593d8c50def27404ac4425b104
ANMOLSCRIPT <anmolgarg1268@gmail.com>
2024-09-26 19:23:52 +0530

    Designed Introduction Page

--- app/src/main/java/com/example/enactmart/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/example/enactmart/fragments/loginRegister/LoginFragment.kt
@@ -3,5 +3,5 @@ package com.example.enactmart.fragments.loginRegister
 import androidx.fragment.app.Fragment
 import com.example.enactmart.R
 
-class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+class LoginFragment: Fragment(R.layout.fragment_login) {
 }
--- app/src/main/java/com/example/enactmart/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/enactmart/fragments/loginRegister/RegisterFragment.kt
@@ -3,5 +3,5 @@ package com.example.enactmart.fragments.loginRegister
 import androidx.fragment.app.Fragment
 import com.example.enactmart.R
 
-class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+class RegisterFragment: Fragment(R.layout.fragment_register) {
 }
